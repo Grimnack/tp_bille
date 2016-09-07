fef8ab2fea3bdc3da3f99c1538fdd4f3566d43a8
Caron Matthieu <caron@a10p11.fil.univ-lille1.fr>
2016-09-07 14:18:05 +0200

    suppression de demi tour et ajout de impact ainsi que modification de grille

--- Environnement.py
+++ Environnement.py
@@ -1,6 +1,6 @@
 class Environnement(object):
     """docstring for Environnement"""
-    def __init__(self, grille):
+    def __init__(self, tailleX, tailleY):
         super(Environnement, self).__init__()
-        self.grille = grille
+        self.grille = [[None] * tailleX for _ in range(tailleY)]
         
